0c93f3871a4000131326e48b1b50687a99034c50
jcockfie <jeremy.cockfield@gmail.com>
2018-01-12 16:00:49 -0600

    added new line after unitClass warning

--- python/hedvalidation/hedvalidation/warning_reporter.py
+++ python/hedvalidation/hedvalidation/warning_reporter.py
@@ -27,6 +27,6 @@ def report_warning_type(warning_type, tag='', default_unit=''):
     """
     warning_types = {
         'cap': '\tWARNING: First word not capitalized or camel case - "%s"\n' % tag,
-        'unitClass': '\tWARNING: No unit specified. Using "%s" as the default - "%s"' % (default_unit, tag)
+        'unitClass': '\tWARNING: No unit specified. Using "%s" as the default - "%s"\n' % (default_unit, tag)
     }
     return warning_types.get(warning_type, None);
