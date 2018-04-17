dcd50eb4128b2f8ad5a7f53c6d6c72af63b792bb
jcockfie <jeremy.cockfield@gmail.com>
2018-04-17 16:23:51 -0500

    added a commaValid error type

--- python/hedvalidation/hedvalidation/error_reporter.py
+++ python/hedvalidation/hedvalidation/error_reporter.py
@@ -8,8 +8,8 @@ Created on Oct 2, 2017
 '''
 
 
-def report_error_type(error_type, error_row=1, error_column=1, tag='', tag_prefix='', unit_class_units='',
-                      opening_bracket_count=0, closing_bracket_count=0):
+def report_error_type(error_type, error_row=1, error_column=1, tag='', tag_prefix='', previous_tag='',
+                      unit_class_units='', opening_bracket_count=0, closing_bracket_count=0):
     """Reports the abc error based on the type of error.
 
     Parameters
@@ -24,6 +24,8 @@ def report_error_type(error_type, error_row=1, error_column=1, tag='', tag_prefi
         The tag that generated the error. The original tag not the formatted one.
     tag_prefix: string
         The tag prefix that generated the error.
+    previous_tag: string
+        The previous tag that potentially could have generated the error. This is passed in with the tag.
     unit_class_units: string
         The unit class units that are associated with the error.
     opening_bracket_count: int
@@ -40,11 +42,13 @@ def report_error_type(error_type, error_row=1, error_column=1, tag='', tag_prefi
         'bracket': '\tERROR: Number of opening and closing brackets are unequal. %s opening brackets. %s '
                    'closing brackets\n' % (opening_bracket_count, closing_bracket_count),
         'comma': '\tERROR: Comma missing after - \"%s\"\n' % tag,
+        'commaValid': '\tERROR: Either \"%s\" contains a comma when it should not or \"%s\" is not a valid tag'
+                      % (previous_tag, tag),
         'duplicate': '\tERROR: Duplicate tag - \"%s\"\n' % tag,
         'isNumeric': '\tERROR: Invalid numeric tag - \"%s\"\n' % tag,
         'row': 'Issues in row %s:\n' % str(error_row),
         'column': 'Issues in row %s column %s:\n' % (str(error_row), str(error_column)),
-        'requireChild':'\tERROR: Descendant tag required - \"%s\"\n' % tag,
+        'requireChild': '\tERROR: Descendant tag required - \"%s\"\n' % tag,
         'tilde': '\tERROR: Too many tildes - group \"%s\"\n' % tag,
         'unique': '\tERROR: Multiple unique tags with prefix - \"%s\"\n' % tag_prefix,
         'unitClass': '\tERROR: Invalid unit - \"%s\" valid units are "%s"\n' % (tag, unit_class_units),
