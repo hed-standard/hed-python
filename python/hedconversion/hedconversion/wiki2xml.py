adabeaf365e28871f471f70f2a92390689a4bdbd
jcockfie <jeremy.cockfield@gmail.com>
2018-06-09 21:43:56 -0500

    refactored code

--- python/hedconversion/hedconversion/wiki2xml.py
+++ python/hedconversion/hedconversion/wiki2xml.py
@@ -5,79 +5,50 @@ Created on Feb 27, 2017
 
 @author: Jeremy Cockfield 
 '''
+from hedemailer import utils;
 from hedconversion import parsewiki;
-import os;
 import tempfile;
-import urllib;
+
 HED_WIKI_URL = 'https://raw.githubusercontent.com/wiki/BigEEGConsortium/HED-Schema/HED-Schema.mediawiki'
 
-# Write data from a URL into a file     
-def url_to_file(file_url, file_location):
-    wiki_url_opener = urllib.URLopener();
-    wiki_url_opener.retrieve(file_url, file_location);
-    wiki_url_opener.close();
 
 # Downloads the wiki HED schema from github
 def download_hed_wiki(wiki_file_location):
-    url_to_file(HED_WIKI_URL, wiki_file_location);
+    utils.url_to_file(HED_WIKI_URL, wiki_file_location);
     return wiki_file_location;
 
-# Writes a XML element tree object into a XML file 
+
+# Writes a XML element tree object into a XML file
 def write_xml_tree_2_xml_file(xml_tree, xml_file_location):
-    xml_file = None;
-    try:
-        xml_file = open(xml_file_location, 'w');
+    with open(xml_file_location, 'w') as xml_file:
         xml_string = parsewiki.prettify(xml_tree);
         xml_file.write(xml_string);
-    finally:
-        if xml_file: 
-            xml_file.close();
-    return xml_file_location;
 
-# Deletes the file if it exist
-def delete_file_if_exist(file_location):
-    file_exist = False;
-    if os.path.isfile(file_location):
-        os.remove(file_location); 
-        file_exist = True;   
-    return file_exist;
 
-# Converts the HED wiki schema into a XML file.    
+# Converts the HED wiki schema into a XML file.
 def convert_hed_wiki_2_xml():
-    hed_info_dictionary = {};
-    hed_wiki_file_location = '';
-    hed_xml_file_location = '';
-    try:
-        hed_wiki_file_location = create_hed_wiki_file();
-        hed_xml_file_location, hed_xml_tree = create_hed_xml_file(hed_wiki_file_location);
-        hed_change_log = parsewiki.get_hed_change_log(hed_wiki_file_location);
-        hed_info_dictionary = {"hed_xml_tree":hed_xml_tree,"hed_change_log":hed_change_log,
-                               "hed_wiki_file_location":hed_wiki_file_location,
-                               "hed_xml_file_location":hed_xml_file_location};
-    except:
-        delete_file_if_exist(hed_wiki_file_location);
-        delete_file_if_exist(hed_xml_file_location);      
+    hed_wiki_file_location = create_hed_wiki_file();
+    hed_xml_file_location, hed_xml_tree = create_hed_xml_file(hed_wiki_file_location);
+    hed_change_log = parsewiki.get_hed_change_log(hed_wiki_file_location);
+    hed_info_dictionary = {"hed_xml_tree": hed_xml_tree, "hed_change_log": hed_change_log,
+                           "hed_wiki_file_location": hed_wiki_file_location,
+                           "hed_xml_file_location": hed_xml_file_location};
     return hed_info_dictionary;
 
-# Creates a HED XML file from a HED wiki schema. 
-def create_hed_xml_file(hed_wiki_file_location, hed_file_location=''):
-        if not hed_file_location:
-            hed_xml_file = tempfile.NamedTemporaryFile(delete=False);
-        else:
-            hed_xml_file = open(hed_file_location, 'w');
-        try:
-            hed_xml_file_location = hed_xml_file.name;
-            hed_xml_tree = parsewiki.hed_wiki_2_xml_tree(hed_wiki_file_location);
-            xml_string = parsewiki.prettify(hed_xml_tree);
-            hed_xml_file.write(xml_string);
-        finally:
-            hed_xml_file.close();
+
+# Creates a HED XML file from a HED wiki schema.
+def create_hed_xml_file(hed_wiki_file_location):
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as hed_xml_file:
+        hed_xml_file_location = hed_xml_file.name;
+        hed_xml_tree = parsewiki.hed_wiki_2_xml_tree(hed_wiki_file_location);
+        xml_string = parsewiki.prettify(hed_xml_tree);
+        hed_xml_file.write(xml_string);
         return hed_xml_file_location, hed_xml_tree;
 
-# Creates a HED wiki schema file from the github wiki schemas.  
+
+# Creates a HED wiki schema file from the github wiki schemas.
 def create_hed_wiki_file():
-        hed_wiki_file = tempfile.NamedTemporaryFile(delete=False);
+    with tempfile.NamedTemporaryFile(delete=False) as hed_wiki_file:
         hed_wiki_file_location = hed_wiki_file.name;
         download_hed_wiki(hed_wiki_file_location);
-        hed_wiki_file.close();
         return hed_wiki_file_location;
