218f4a6675132b1ace2b08f940c902460ef6965a
Dung Truong <dt.young112@gmail.com>
2019-07-26 14:41:39 -0700

    CSRF protect with POST for EEG validation

--- webinterface/webinterface/constants/routing/page_constants.py
+++ webinterface/webinterface/constants/routing/page_constants.py
@@ -2,3 +2,4 @@ ADDITIONAL_EXAMPLES_PAGE = 'additional-examples.html';
 HELP_PAGE = 'help.html';
 HOME_PAGE = 'home.html';
 VALIDATION_PAGE = 'validation.html';
+EEG_VALIDATION_PAGE = 'eegvalidation.html';
