fde92a332951ddaa4f01933ac0626fc81c8e9301
Kay Robbins <1189050+VisLab@users.noreply.github.com>
2020-07-04 09:40:28 -0500

    Fixed the examples of running validators with files

--- hedvalidation/examples/hed_string_examples.py
+++ hedvalidation/examples/hed_string_examples.py
@@ -13,43 +13,44 @@ if __name__ == '__main__':
                    'Event/Category/Participant response, ' \
                    '(Participant ~ Action/Button press/Keyboard ~ Participant/Effect/Body part/Arm/Hand/Finger)'
     hed_input_reader = HedInputReader(hed_string_1, hed_xml_file=local_hed_file)
-    print(hed_input_reader.get_printable_issue_string('Example 1 should have no issues with HEDv7.1.1'))
+    print(hed_input_reader.get_printable_issue_string(
+        '[Example 1a] hed_string_1 should have no issues with HEDv7.1.1'))
 
     # Example 1b: Try with the latest version of HED.xml
     hed_input_reader = HedInputReader(hed_string_1)
-    print(hed_input_reader.get_printable_issue_string('Example 1 issues with the latest HED version'))
+    print(hed_input_reader.get_printable_issue_string(
+        '[Example 1b] hed_string_1 probably has noissues with the latest HED version'))
 
-    hed_string_2 = "Event/Label/TakeSurvey, Event/Category/Experimental stimulus/Instruction, " \
-                   "Event/Description/Tell subject to start survey, (Participant, Action/Deny)"
-
-    # Example 2: Invalid HED string (junk in last tag)
-    hed_string_2 = 'Event/Category/Participant response, ' \
+    # Example 2a: Invalid HED string (junk in last tag)
+    hed_string_2 = 'Event/Category/Participant response,'  \
                    '(Participant ~ Action/Button press/Keyboard ~ Participant/Effect/Body part/Arm/Hand/Finger),' \
                    'dskfjkf/dskjdfkj/sdkjdsfkjdf/sdlfdjdsjklj'
     hed_input_reader = HedInputReader(hed_string_2)
-    print(hed_input_reader.get_printable_issue_string('Example 2 has junk in the last tag'))
+    print(hed_input_reader.get_printable_issue_string('[Example 2a] hed_string_2 has junk in the last tag'))
 
-    # Example 2a: However HED string of Example 2 has valid syntax so syntactic validation works
+    # Example 2b: However HED string of Example 2 has valid syntax so syntactic validation works
     hed_input_reader = HedInputReader(hed_string_2, run_semantic_validation=False)
-    print(hed_input_reader.get_printable_issue_string('Example 2 is syntactically correct'))
+    print(hed_input_reader.get_printable_issue_string('[Example 2b] hed_string_2 is syntactically correct'))
 
-    # Example 3: Invalid HED string
+    # Example 3a: Invalid HED string
     hed_string_3 = 'Event/Description/The start of increasing the size of sector, Event/Label/Sector start, ' \
                    'Event/Category/Experimental stimulus/Instruction/Attend, ' \
                    '(Item/2D shape/Sector, Attribute/Visual/Color/Red) ' \
                    '(Item/2D shape/Ellipse/Circle, Attribute/Visual/Color / Red), Sensory presentation/Visual, ' \
                    'Participant/Efffectt/Visual, Participant/Effect/Cognitive/Target'
     hed_input_reader = HedInputReader(hed_string_3)
-    print(hed_input_reader.get_printable_issue_string('Example 3 has missing comma so fails before Efffectt typo'))
+    print(hed_input_reader.get_printable_issue_string(
+        '[Example 3a] hed_string_3 has missing comma so fails before Efffectt typo'))
 
-    # Example 3a: Using issue list directly - issues are returned as a list of dictionaries
+    # Example 3b: Using issue list directly - issues are returned as a list of dictionaries
     issues = hed_input_reader.get_validation_issues()
-    print('hed_string_4 has ', len(issues), ' issues\n')
+    print('[Example 3b] hed_string_3 has ', len(issues), ' issues\n')
 
-    # Example 4: Example using the ~ notation
+    # Example 4a: Example using the ~ notation
     hed_string_4 = 'Event/Label/ButtonDeny, Event/Description/Button push to deny access to the ID holder, ' \
                    'Event/Category/Participant response, ' \
                    '(Participant ~ Action/Button press/Keyboard ~ Participant/Effect/Body part/Arm/Hand/Finger), ' \
                    '(Participant ~ Action/Deny/Access ~ Item/Object/Person/IDHolder)'
     hed_input_reader = HedInputReader(hed_string_4, hed_xml_file=local_hed_file)
-    print(hed_input_reader.get_printable_issue_string('Example 4 the ~ notation works in v7.1.1'))
+    print(hed_input_reader.get_printable_issue_string(
+        '[Example 4a] the ~ notation in hed_string_4 works in v7.1.1'))
