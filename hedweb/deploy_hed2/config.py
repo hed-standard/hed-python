68b5c62d925eb4317932ae65dcce205c62329fef
Kay Robbins <1189050+VisLab@users.noreply.github.com>
2020-11-08 18:27:01 -0600

    Added separate deployment directories for hed2 and hed3

--- hedweb/deploy_hed2/config.py
+++ hedweb/deploy_hed2/config.py
@@ -20,7 +20,7 @@ class Config(object):
     STATIC_URL_PATH_ATTRIBUTE_NAME = 'STATIC_URL_PATH'
     UPLOAD_FOLDER = os.path.join(tempfile.gettempdir(), 'hedtools_uploads')
     URL_PREFIX = None
-
+    HED_CACHE_FOLDER = '/var/cache/hed_cache'
 
 class DevelopmentConfig(Config):
     DEBUG = False
@@ -30,8 +30,8 @@ class DevelopmentConfig(Config):
 class ProductionConfig(Config):
     DEBUG = False
     TESTING = False
-	URL_PREFIX = '/hedschema';
-    STATIC_URL_PATH = '/hedschema/static';
+	URL_PREFIX = 'hed'
+	STATIC_URL_PATH = '/hed/static'
 
 
 class TestConfig(Config):
