894a0778daab11277d9eaa154af68890f9c6def9
Ian C <ian.callanan@utsa.edu>
2020-12-16 16:40:14 -0600

    Remove a few instances of hedtools3 and replace with hedtools

--- hedweb/config.py
+++ hedweb/config.py
@@ -20,7 +20,7 @@ class Config(object):
     f.close()
     STATIC_URL_PATH = None
     STATIC_URL_PATH_ATTRIBUTE_NAME = 'STATIC_URL_PATH'
-    UPLOAD_FOLDER = os.path.join(tempfile.gettempdir(), 'hedtools3_uploads')
+    UPLOAD_FOLDER = os.path.join(tempfile.gettempdir(), 'hedtools_uploads')
     URL_PREFIX = None
     HED_CACHE_FOLDER = os.path.join(BASE_DIRECTORY, 'HED_CACHE')
 
--- hedweb/config_template.py
+++ hedweb/config_template.py
@@ -7,18 +7,18 @@ import tempfile
 
 
 class Config(object):
-    LOG_DIRECTORY = '/var/log/hedtools3'
+    LOG_DIRECTORY = '/var/log/hedtools'
     LOG_FILE = os.path.join(LOG_DIRECTORY, 'error.log')
-    if not os.path.exists('/var/log/hedtools3/tmp.txt'):
-        f = open('/var/log/hedtools3/tmp.txt', 'w+')
+    if not os.path.exists('/var/log/hedtools/tmp.txt'):
+        f = open('/var/log/hedtools/tmp.txt', 'w+')
         f.write(str(os.urandom(24)))
         f.close()
-    f = open('/var/log/hedtools3/tmp.txt', 'r')
+    f = open('/var/log/hedtools/tmp.txt', 'r')
     SECRET_KEY = f.read()  # os.getenv('SECRET_KEY') # os.urandom(24)
     f.close()
     STATIC_URL_PATH = None
     STATIC_URL_PATH_ATTRIBUTE_NAME = 'STATIC_URL_PATH'
-    UPLOAD_FOLDER = os.path.join(tempfile.gettempdir(), 'hedtools3_uploads')
+    UPLOAD_FOLDER = os.path.join(tempfile.gettempdir(), 'hedtools_uploads')
     URL_PREFIX = None
     HED_CACHE_FOLDER = None
 
