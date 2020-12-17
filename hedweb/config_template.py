5585dfc31a61b46c4286f8f82aeda6805be3f05b
Ian C <ian.callanan@utsa.edu>
2020-12-17 12:43:45 -0600

    Update config template, delete root config file.

--- hedweb/config_template.py
+++ hedweb/config_template.py
@@ -1,5 +1,8 @@
 """
 This module contains the default configurations for hedwebmap
+
+If you want to locally run debug code, create a copy of this file called
+config.py and set the BASE_DIRECTORY at the top of it to be valid.
 """
 
 import os
@@ -7,25 +10,30 @@ import tempfile
 
 
 class Config(object):
-    LOG_DIRECTORY = '/var/log/hedtools'
+    # Folder the program stores hed cache, log, etc.
+    BASE_DIRECTORY = '/path/to/base/folder'
+    LOG_DIRECTORY = os.path.join(BASE_DIRECTORY, 'log')
+    os.makedirs(LOG_DIRECTORY, exist_ok=True)
     LOG_FILE = os.path.join(LOG_DIRECTORY, 'error.log')
-    if not os.path.exists('/var/log/hedtools/tmp.txt'):
-        f = open('/var/log/hedtools/tmp.txt', 'w+')
+    KEY_FILE = os.path.join(LOG_DIRECTORY, 'tmp.txt')
+    if not os.path.exists(KEY_FILE):
+        f = open(KEY_FILE, 'w+')
         f.write(str(os.urandom(24)))
         f.close()
-    f = open('/var/log/hedtools/tmp.txt', 'r')
+    f = open(KEY_FILE, 'r')
     SECRET_KEY = f.read()  # os.getenv('SECRET_KEY') # os.urandom(24)
     f.close()
     STATIC_URL_PATH = None
     STATIC_URL_PATH_ATTRIBUTE_NAME = 'STATIC_URL_PATH'
     UPLOAD_FOLDER = os.path.join(tempfile.gettempdir(), 'hedtools_uploads')
     URL_PREFIX = None
-    HED_CACHE_FOLDER = None
+    HED_CACHE_FOLDER = os.path.join(BASE_DIRECTORY, 'HED_CACHE')
 
 
 class DevelopmentConfig(Config):
     DEBUG = False
     TESTING = False
+
 
 class TestConfig(Config):
     DEBUG = False
@@ -35,6 +43,7 @@ class TestConfig(Config):
 class ProductionConfig(Config):
     DEBUG = False
     TESTING = False
+
 
 class DebugConfig(Config):
     DEBUG = True
