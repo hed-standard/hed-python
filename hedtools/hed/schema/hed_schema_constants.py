8cfcffa8b2448d3a1bacbdc793e48d246f944735
Kay Robbins <1189050+VisLab@users.noreply.github.com>
2021-03-14 07:25:08 -0500

    Updated attributes to included allowedCharacter placeholder

--- hedtools/hed/schema/hed_schema_constants.py
+++ hedtools/hed/schema/hed_schema_constants.py
@@ -1,5 +1,6 @@
 # These need to match the attributes/element name/etc used to load from the xml
 class HedKey:
+    AllowedCharacter = 'allowedCharacter'
     Default = 'default'
     ExtensionAllowed = 'extensionAllowed'
     # On opening, the extension allowed attribute is propagated down to child tags.
@@ -39,16 +40,17 @@ class HedKey:
 # These are the only ones directly loaded from or saved to files.
 TAG_ATTRIBUTE_KEYS = [HedKey.TakesValue, HedKey.IsNumeric, HedKey.Recommended,
                       HedKey.RequireChild, HedKey.RequiredPrefix, HedKey.Unique, HedKey.PredicateType,
-                      HedKey.Position, HedKey.UnitClass, HedKey.Default, HedKey.ExtensionAllowed]
+                      HedKey.Position, HedKey.UnitClass, HedKey.Default, HedKey.ExtensionAllowed,
+                      HedKey.AllowedCharacter]
 # for these it gets the value of the attribute, rather than treating it as a bool.
-STRING_ATTRIBUTE_DICTIONARY_KEYS = [HedKey.Default, HedKey.UnitClass,
+STRING_ATTRIBUTE_DICTIONARY_KEYS = [HedKey.AllowedCharacter, HedKey.Default, HedKey.UnitClass,
                                     HedKey.Position, HedKey.PredicateType, HedKey.DefaultUnits]
 # List of all keys that are for tags, including ones derived from other attributes.
 ALL_TAG_DICTIONARY_KEYS = [*TAG_ATTRIBUTE_KEYS, HedKey.ExtensionAllowedPropagated, HedKey.AllTags]
 UNIT_CLASS_DICTIONARY_KEYS = [HedKey.SIUnit, HedKey.UnitSymbol]
 UNIT_MODIFIER_DICTIONARY_KEYS = [HedKey.SIUnitModifier, HedKey.SIUnitSymbolModifier]
 
-UNIT_CLASS_ATTRIBUTES = [HedKey.DefaultUnits, HedKey.SIUnit, HedKey.UnitSymbol]
+UNIT_CLASS_ATTRIBUTES = [HedKey.DefaultUnits, HedKey.SIUnit, HedKey.UnitSymbol, HedKey.AllowedCharacter]
 UNIT_MODIFIER_ATTRIBUTES = [HedKey.SIUnitModifier, HedKey.SIUnitSymbolModifier]
 
 VERSION_ATTRIBUTE = 'version'
