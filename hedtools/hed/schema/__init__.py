84477b5bf005d7edea48d91a676e4b2de0da380b
Kay Robbins <1189050+VisLab@users.noreply.github.com>
2021-06-01 14:34:22 -0500

    Fixed the imports that had schema conflicts

--- hedtools/hed/schema/__init__.py
+++ hedtools/hed/schema/__init__.py
@@ -2,4 +2,5 @@ from hed.schema.hed_schema import HedSchema
 from hed.schema.hed_schema_file import load_schema, load_schema_version, from_string, get_hed_xml_version, \
     convert_schema_to_format
 from hed.schema.hed_schema_constants import HedKey
-from hed.schema.hed_cache import cache_all_hed_xml_versions, get_all_hed_versions, set_cache_directory
+from hed.schema.hed_cache import cache_all_hed_xml_versions, get_all_hed_versions, \
+    get_path_from_hed_version, set_cache_directory
