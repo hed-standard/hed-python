7e8d43ee976fe6c2a2c89d0f5575262032511ff4
Kay Robbins <1189050+VisLab@users.noreply.github.com>
2021-09-03 16:34:59 -0500

    Updated the data for the test cases

---
+++ webtools/tests/test_events.py
@@ -0,0 +1,152 @@
+import os
+import shutil
+import unittest
+from werkzeug.test import create_environ
+from werkzeug.wrappers import Request
+
+from hed import schema as hedschema
+from hed import models
+from hed.errors.exceptions import HedFileError
+from hedweb.constants import common
+from hedweb.app_factory import AppFactory
+
+
+class Test(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.upload_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/upload')
+        app = AppFactory.create_app('config.TestConfig')
+        with app.app_context():
+            from hedweb.routes import route_blueprint
+            app.register_blueprint(route_blueprint)
+            if not os.path.exists(cls.upload_directory):
+                os.mkdir(cls.upload_directory)
+            app.config['UPLOAD_FOLDER'] = cls.upload_directory
+            cls.app = app
+            cls.app.test = app.test_client()
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.upload_directory)
+
+    def test_get_input_from_events_form_empty(self):
+        from hedweb.events import get_input_from_events_form
+        self.assertRaises(TypeError, get_input_from_events_form, {},
+                          "An exception should be raised if an empty request is passed")
+        self.assertTrue(1, "Testing get_input_from_events_form")
+
+    def test_get_input_from_events_form(self):
+        from hed.models import EventsInput
+        from hed.schema import HedSchema
+        from hedweb.events import get_input_from_events_form
+        with self.app.test:
+            json_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), './data/bids_events.json')
+            events_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), './data/bids_events.tsv')
+            with open(json_path, 'rb') as fp:
+                with open(events_path, 'rb') as fpe:
+                    environ = create_environ(data={common.JSON_FILE: fp, common.SCHEMA_VERSION: '8.0.0',
+                                             common.EVENTS_FILE: fpe, common.DEFS_EXPAND: 'on',
+                                             common.COMMAND_OPTION: common.COMMAND_ASSEMBLE})
+            request = Request(environ)
+            arguments = get_input_from_events_form(request)
+            self.assertIsInstance(arguments[common.EVENTS], EventsInput,
+                                  "get_input_from_events_form should have an events object")
+            self.assertIsInstance(arguments[common.SCHEMA], HedSchema,
+                                  "get_input_from_events_form should have a HED schema")
+            self.assertEqual(common.COMMAND_ASSEMBLE, arguments[common.COMMAND],
+                             "get_input_from_events_form should have a command")
+            self.assertTrue(arguments[common.DEFS_EXPAND],
+                            "get_input_from_events_form should have defs_expand true when on")
+
+    def test_events_process_empty_file(self):
+        # Test for empty events_path
+        from hedweb.events import events_process
+        arguments = {'events_path': ''}
+        try:
+            events_process(arguments)
+        except HedFileError:
+            pass
+        except Exception:
+            self.fail('events_process threw the wrong exception when events_path was empty')
+        else:
+            self.fail('events_process should have thrown a HedFileError exception when events_path was empty')
+
+    def test_events_process(self):
+        from hedweb.events import events_process
+        events_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/bids_events.tsv')
+        json_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/bids_events.json')
+        schema_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/HED8.0.0.xml')
+        hed_schema = hedschema.load_schema(hed_file_path=schema_path)
+        json_sidecar = models.Sidecar(file=json_path, name='bids_json')
+        events = models.EventsInput(file=events_path, sidecars=json_sidecar, name='bids_events')
+        arguments = {common.EVENTS: events, common.COMMAND: common.COMMAND_VALIDATE, common.DEFS_EXPAND: True,
+                     common.CHECK_FOR_WARNINGS: True, common.SCHEMA: hed_schema}
+        with self.app.app_context():
+            results = events_process(arguments)
+            self.assertTrue(isinstance(results, dict),
+                            'events_process validation should return a result dictionary when validation errors')
+            self.assertEqual('success', results['msg_category'],
+                             'events_process validate should return success if no errors')
+
+        schema_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/HED7.2.0.xml')
+        arguments[common.SCHEMA] = hedschema.load_schema(hed_file_path=schema_path)
+        with self.app.app_context():
+            results = events_process(arguments)
+            self.assertTrue(isinstance(results, dict),
+                            'events_process validation should return a dictionary when validation errors')
+            self.assertEqual('warning', results['msg_category'],
+                             'events_process validate should give warning when errors')
+            self.assertTrue(results["data"], 'events_process validate should return data when errors')
+
+    def test_events_assemble(self):
+        from hedweb.events import events_assemble
+        events_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/bids_events.tsv')
+        json_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/bids_events.json')
+        schema_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/HED8.0.0.xml')
+        hed_schema = hedschema.load_schema(hed_file_path=schema_path)
+        json_sidecar = models.Sidecar(file=json_path, name='bids_json')
+        events = models.EventsInput(file=events_path, sidecars=json_sidecar,name='bids_events')
+        with self.app.app_context():
+            results = events_assemble(hed_schema, events, defs_expand=True)
+            self.assertTrue('data' in results,
+                            'events_assemble results should have a data key when no errors')
+            self.assertEqual('success', results["msg_category"],
+                             'events_assemble msg_category should be success when no errors')
+
+        schema_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/HED7.2.0.xml')
+        hed_schema = hedschema.load_schema(hed_file_path=schema_path)
+        with self.app.app_context():
+            results = events_assemble(hed_schema, events, defs_expand=True)
+            self.assertTrue(results['data'],
+                            'events_assemble results should have a data key when errors')
+            self.assertEqual('warning', results['msg_category'],
+                             'events_assemble msg_category should be warning when errors')
+
+    def test_events_validate(self):
+        from hedweb.events import events_validate
+        events_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/bids_events.tsv')
+        json_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/bids_events.json')
+        schema_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/HED7.2.0.xml')
+        hed_schema = hedschema.load_schema(hed_file_path=schema_path)
+        json_sidecar = models.Sidecar(file=json_path, name='bids_json')
+        events = models.EventsInput(file=events_path, sidecars=json_sidecar, name='bids_events')
+        with self.app.app_context():
+            results = events_validate(hed_schema, events)
+            self.assertTrue(results['data'],
+                            'events_validate results should have a data key when validation errors')
+            self.assertEqual('warning', results["msg_category"],
+                             'events_validate msg_category should be warning when errors')
+
+        schema_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/HED8.0.0.xml')
+        hed_schema = hedschema.load_schema(hed_file_path=schema_path)
+
+        with self.app.app_context():
+            results = events_validate(hed_schema, events)
+            self.assertFalse(results['data'],
+                             'events_validate results should not have a data key when no validation errors')
+            self.assertEqual('success', results['msg_category'],
+                             'events_validate msg_category should be success when no errors')
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ webtools/tests/test_schema.py
@@ -0,0 +1,116 @@
+import os
+import shutil
+import unittest
+from werkzeug.test import create_environ
+from werkzeug.wrappers import Request
+
+from hedweb.app_factory import AppFactory
+
+
+class Test(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.upload_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/upload')
+        app = AppFactory.create_app('config.TestConfig')
+        with app.app_context():
+            from hedweb.routes import route_blueprint
+            app.register_blueprint(route_blueprint)
+            if not os.path.exists(cls.upload_directory):
+                os.mkdir(cls.upload_directory)
+            app.config['UPLOAD_FOLDER'] = cls.upload_directory
+            cls.app = app
+            cls.app.test = app.test_client()
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.upload_directory)
+
+    def test_generate_input_from_schema_form_empty(self):
+        from hedweb.schema import get_input_from_schema_form
+        self.assertRaises(TypeError, get_input_from_schema_form, {},
+                          "An exception is raised if an empty request is passed to generate_input_from_schema")
+
+    def test_get_input_from_schema_form_valid(self):
+        from hed.schema import HedSchema
+        from hedweb.constants import common
+        from hedweb.schema import get_input_from_schema_form
+        with self.app.test:
+            schema_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), './data/HED8.0.0.xml')
+            with open(schema_path, 'rb') as fp:
+                environ = create_environ(data={common.SCHEMA_FILE: fp,
+                                               common.SCHEMA_UPLOAD_OPTIONS: common.SCHEMA_FILE_OPTION,
+                                               common.COMMAND_OPTION:  common.COMMAND_CONVERT})
+            request = Request(environ)
+            arguments = get_input_from_schema_form(request)
+            self.assertIsInstance(arguments[common.SCHEMA], HedSchema,
+                                  "get_input_from_schema_form should have a HED schema")
+            self.assertEqual(common.COMMAND_CONVERT, arguments[common.COMMAND],
+                             "get_input_from_schema_form should have a command")
+            self.assertFalse(arguments[common.CHECK_FOR_WARNINGS],
+                            "get_input_from_schema_form should have check_for_warnings false when not given")
+
+    def test_schema_process(self):
+        from hedweb.schema import schema_process
+        from hed.errors.exceptions import HedFileError
+        arguments = {'schema_path': ''}
+        try:
+            a = schema_process(arguments)
+        except HedFileError:
+            pass
+        except Exception:
+            self.fail('schema_process threw the wrong exception when schema_path was empty')
+        else:
+            self.fail('schema_process should have thrown a HedFileError exception when schema_path was empty')
+
+    def test_schema_check(self):
+        from hedweb.schema import schema_validate
+        from hed import schema as hedschema
+        schema_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/HED7.2.0.xml')
+        hed_schema = hedschema.load_schema(hed_file_path=schema_path)
+        display_name = 'HED7.2.0.xml'
+        with self.app.app_context():
+            results = schema_validate(hed_schema, display_name)
+            self.assertTrue(results['data'], "HED 7.2.0 is not HED-3G compliant")
+
+        schema_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/HED8.0.0.xml')
+        hed_schema = hedschema.load_schema(hed_file_path=schema_path)
+        display_name = 'HED8.0.0.xml'
+        with self.app.app_context():
+            results = schema_validate(hed_schema, display_name)
+            self.assertFalse(results['data'], "HED8.0.0 is HED-3G compliant")
+
+    def test_schema_convert(self):
+        from hedweb.schema import schema_convert
+        from hed import schema as hedschema
+        from hed.errors.exceptions import HedFileError
+
+        schema_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/HED7.2.0.xml')
+        hed_schema = hedschema.load_schema(hed_file_path=schema_path)
+        display_name = 'HED7.2.0.xml'
+        with self.app.app_context():
+            results = schema_convert(hed_schema, display_name)
+            self.assertTrue(results['data'], "HED 7.2.0.xml can be converted to mediawiki")
+
+        schema_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/HED8.0.0.xml')
+        hed_schema = hedschema.load_schema(hed_file_path=schema_path)
+        display_name = 'HED8.0.0.4.xml'
+        with self.app.app_context():
+            results = schema_convert(hed_schema, display_name)
+            self.assertTrue(results['data'], "HED 8.0.0.xml can be converted to mediawiki")
+
+        schema_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/HEDbad.xml')
+        display_name = 'HEDbad.xml'
+        with self.app.app_context():
+            try:
+                hed_schema = hedschema.load_schema(hed_file_path=schema_path)
+                results = schema_convert(hed_schema, display_name)
+            except HedFileError:
+                pass
+            except Exception:
+                self.fail('schema_convert threw Exception instead of HedFileError for invalid schema file header')
+            else:
+                self.fail('schema_process should throw HedFileError when the schema file header was invalid')
+
+
+if __name__ == '__main__':
+    unittest.main()
