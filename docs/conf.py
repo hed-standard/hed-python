7794a4311c1669cb233adfb1989eec1dc83044dd
Kay Robbins <1189050+VisLab@users.noreply.github.com>
2021-09-17 07:20:09 -0500

    Updated the sphinx docs and removed

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../hedtools/hed/'))
-sys.path.insert(0, os.path.abspath('../wedtools/hedweb/'))
+sys.path.insert(0, os.path.abspath('../../hedtools/hed/'))
+sys.path.insert(0, os.path.abspath('../../wedtools/hedweb/'))
 
 # -- Project information -----------------------------------------------------
 
