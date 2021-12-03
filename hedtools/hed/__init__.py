7adab1f6f5dfd152ccae8c980990027a126a4e0b
Kay Robbins <1189050+VisLab@users.noreply.github.com>
2021-12-03 17:16:48 -0600

    Renamed the examples directory to hedcode

--- hedtools/hed/__init__.py
+++ hedtools/hed/__init__.py
@@ -12,4 +12,9 @@ from hed.schema.hed_schema import HedSchema
 from hed.schema.hed_schema_group import HedSchemaGroup
 from hed.schema.hed_schema_file import load_schema, load_schema_version
 
+# from hed.tools.data_utils import get_new_dataframe, make_info_dataframe, remove_quotes, reorder_columns, \
+#     separate_columns, make_key, get_key_hash, get_row_hash
+# from hed.tools.event_utils import add_columns, check_match, delete_columns, delete_rows_by_column, \
+#    replace_values
+
 from hed.validator.hed_validator import HedValidator
