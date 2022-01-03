cecde033197de0cbc996d8f141e833521d16e74f
Kay Robbins <1189050+VisLab@users.noreply.github.com>
2022-01-03 10:48:51 -0600

    Minor corrections to bids validation code

--- hedtools/hed/tools/bids_validator.py
+++ hedtools/hed/tools/bids_validator.py
@@ -5,13 +5,8 @@ the get_validation_issues() function.
 
 """
 
-from hed.errors.error_types import ValidationErrors
 from hed.errors.error_reporter import ErrorHandler
 from hed.errors.error_types import ErrorContext
-
-from hed.models.hed_string import HedString
-from hed.validator.tag_validator import TagValidator
-from functools import partial
 
 
 class BidsValidator:
@@ -32,41 +27,39 @@ class BidsValidator:
 
         self._hed_schema = hed_schema
 
-
-    def validate_events(self, bids_validators, name=None, error_handler=None, check_for_warnings=True, **kwargs):
-        """Run the given validators on a BIDS dataset
-         Parameters
-         ----------
-         bids_validators : [func or validator like] or func or validator like
-             A validator or list of validators to apply to the hed strings in this sidecar.
-         name: str
-             If present, will use this as the filename for context, rather than using the actual filename
-             Useful for temp filenames.
-         error_handler : ErrorHandler or None
-             Used to report errors.  Uses a default one if none passed in.
-         check_for_warnings: bool
-             If True this will check for and return warnings as well
-         kwargs:
-             See util.translate_ops or the specific validators for additional options
-         Returns
-         -------
-         validation_issues: [{}]
-             The list of validation issues found
-         """
-        if not name:
-            name = self.name
-        if not isinstance(bids_validators, list):
-            validators = [bids_validators]
-
-        if error_handler is None:
-            error_handler = ErrorHandler()
-
-        error_handler.push_error_context(ErrorContext.FILE_NAME, name)
-        validation_issues = self.get_def_and_mapper_issues(error_handler, check_for_warnings)
-        validators = validators.copy()
-        validation_issues += self._run_validators(validators, error_handler=error_handler,
-                                                  check_for_warnings=check_for_warnings, **kwargs)
-        error_handler.pop_error_context()
-
-        return validation_issues
-
+    # def validate_events(self, bids_validators, name=None, error_handler=None, check_for_warnings=True, **kwargs):
+    #     """Run the given validators on a BIDS dataset
+    #      Parameters
+    #      ----------
+    #      bids_validators : [func or validator like] or func or validator like
+    #          A validator or list of validators to apply to the hed strings in this sidecar.
+    #      name: str
+    #          If present, will use this as the filename for context, rather than using the actual filename
+    #          Useful for temp filenames.
+    #      error_handler : ErrorHandler or None
+    #          Used to report errors.  Uses a default one if none passed in.
+    #      check_for_warnings: bool
+    #          If True this will check for and return warnings as well
+    #      kwargs:
+    #          See util.translate_ops or the specific validators for additional options
+    #      Returns
+    #      -------
+    #      validation_issues: [{}]
+    #          The list of validation issues found
+    #      """
+    #     if not name:
+    #         name = 'Bids_validator'
+    #     if not isinstance(bids_validators, list):
+    #         validators = [bids_validators]
+    #
+    #     if error_handler is None:
+    #         error_handler = ErrorHandler()
+    #
+    #     error_handler.push_error_context(ErrorContext.FILE_NAME, name)
+    #     validation_issues = self.get_def_and_mapper_issues(error_handler, check_for_warnings)
+    #     validators = validators.copy()
+    #     validation_issues += self._run_validators(validators, error_handler=error_handler,
+    #                                               check_for_warnings=check_for_warnings, **kwargs)
+    #     error_handler.pop_error_context()
+    #
+    #     return validation_issues
