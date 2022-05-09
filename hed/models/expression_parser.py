c5e681763ea8d8241f7028816de11a8a84b6f70c
IanCa <ianrcallanan@gmail.com>
2022-05-08 20:59:37 -0500

    First pass at an expression parser for tag searching

---
+++ hed/models/expression_parser.py
@@ -0,0 +1,373 @@
+import copy
+import re
+import os
+
+print("Number of CPUs:", os.cpu_count())
+pid = 0
+affinity = os.sched_getaffinity(pid)
+os.sched_setaffinity(pid, [0, 1, 2, 3])
+
+
+# Syntax:
+# and = "and" or ","
+# or = "or"
+# logical operation grouping = "(" and ")"
+# inexact grouping = "[" and "]"
+# same group grouping = "[[" and "]]"
+# not = "~"
+
+class Token:
+    And = 0
+    Tag = 1
+    ExactGroup = 2
+    ExactGroupEnd = 3
+    ContainingGroup = 8
+    ContainingGroupEnd = 9
+    Or = 4
+    LogicalGroup = 5
+    LogicalGroupEnd = 6
+    LogicalNegation = 7
+
+    def __init__(self, text):
+        tokens = {
+            ",": Token.And,
+            "and": Token.And,
+            "or": Token.Or,
+            "[[": Token.ExactGroup,
+            "]]": Token.ExactGroupEnd,
+            "[": Token.ContainingGroup,
+            "]": Token.ContainingGroupEnd,
+            "(": Token.LogicalGroup,
+            ")": Token.LogicalGroupEnd,
+            "~": Token.LogicalNegation,
+        }
+        self.kind = tokens.get(text, Token.Tag)
+        self.text = text
+
+    def __str__(self):
+        return self.text
+
+    def __eq__(self, other):
+        if self.kind == other:
+            return True
+        return False
+
+
+class Expression:
+    def __init__(self, token, left=None, right=None):
+        self.left = left
+        self.right = right
+        self.token = token
+
+    def __str__(self):
+        output_str = ""
+        if self.left:
+            output_str += str(self.left)
+        output_str += " " + str(self.token)
+        if self.right:
+            output_str += str(self.right)
+        return output_str
+
+    def handle_expr(self, hed_group, exact=False):
+        # groups_found = hed_group.find_exact_tags([self.token.text], recursive=True)
+        groups_found = hed_group.find_tags_with_term(self.token.text, recursive=True, include_groups=1)
+        if exact:
+            all_found_groups = groups_found
+        else:
+            all_found_groups = []
+            for group in groups_found:
+                while group:
+                    all_found_groups.append(group)
+                    group = group._parent
+        return all_found_groups
+
+
+class ExpressionAnd(Expression):
+    def handle_expr(self, hed_group, exact=False):
+        groups1 = self.left.handle_expr(hed_group, exact=exact)
+        if not groups1:
+            return groups1
+        groups2 = self.right.handle_expr(hed_group, exact=exact)
+        return [group for group in groups1 if group in groups2]
+
+
+class ExpressionOr(Expression):
+    def handle_expr(self, hed_group, exact=False):
+        groups1 = self.left.handle_expr(hed_group, exact=exact)
+        # Don't early out as we need to gather all groups incase tags appear more than once etc
+        groups2 = self.right.handle_expr(hed_group, exact=exact)
+        return groups1 + groups2
+
+
+class ExpressionNegation(Expression):
+    def handle_expr(self, hed_group, exact=False):
+        groups = self.right.handle_expr(hed_group, exact=exact)
+
+        # todo: This probably needs fixing
+        negated_groups = [group for group in hed_group.get_all_groups() if group not in groups]
+        # Possible alternate if only groups with tags are valid results(is this a real case?)
+        #negated_groups = [group for group in hed_group.get_all_groups() if group not in groups if group.tags()]
+
+        return negated_groups
+
+
+class ExpressionLogicalGroup(Expression):
+    def handle_expr(self, hed_group, exact=False):
+        return self.right.handle_expr(hed_group, exact=exact)
+
+
+class ExpressionExactGroup(Expression):
+    def handle_expr(self, hed_group, exact=False):
+        result = self.right.handle_expr(hed_group, exact=True)
+        found_groups = result
+        if result:
+            found_parent_groups = list()
+            for group in found_groups:
+                if not group.is_group:
+                    continue
+                if group._parent:
+                    found_parent_groups.append(group._parent)
+
+            if found_parent_groups:
+                return found_parent_groups
+
+        return []
+
+
+class ExpressionContainingGroup(Expression):
+    def handle_expr(self, hed_group, exact=False):
+        result = self.right.handle_expr(hed_group)
+        found_groups = result
+        found_parent_groups = list()
+        if result:
+            for group in found_groups:
+                if not group.is_group:
+                    continue
+                if group._parent:
+                    found_parent_groups.append(group._parent)
+
+
+        if found_parent_groups:
+            return found_parent_groups
+        return []
+
+
+class TagExpressionParser:
+    def __init__(self, expression_string):
+        self.tokens = []
+        self.at_token = -1
+        self.tree = self._parse(expression_string)
+        self._org_string = expression_string
+
+    def get_next_token(self):
+        self.at_token += 1
+        if self.at_token >= len(self.tokens):
+            raise ValueError("Parse error in get next token")
+        return self.tokens[self.at_token]
+
+    def next_token_is(self, kinds):
+        if self.at_token + 1 >= len(self.tokens):
+            return None
+        if self.tokens[self.at_token + 1].kind in kinds:
+            return self.get_next_token()
+        return None
+
+    def handle_and_op(self):
+        expr = self.handle_negation()
+        next_token = self.next_token_is([Token.And, Token.Or])
+        while next_token:
+            right = self.handle_negation()
+            if next_token.kind == Token.And:
+                expr = ExpressionAnd(next_token, expr, right)
+            else:
+                expr = ExpressionOr(next_token, expr, right)
+            next_token = self.next_token_is([Token.And, Token.Or])
+
+        return expr
+
+    def handle_negation(self):
+        next_token = self.next_token_is([Token.LogicalNegation])
+        if next_token == Token.LogicalNegation:
+            interior = self.handle_grouping_op()
+            expr = ExpressionNegation(next_token, right=interior)
+            return expr
+        else:
+            return self.handle_grouping_op()
+
+    def handle_grouping_op(self):
+        next_token = self.next_token_is([Token.ExactGroup, Token.LogicalGroup, Token.ContainingGroup])
+        if next_token == Token.ExactGroup:
+            interior = self.handle_and_op()
+            expr = ExpressionExactGroup(next_token, right=interior)
+            next_token = self.next_token_is([Token.ExactGroupEnd])
+            if next_token != Token.ExactGroupEnd:
+                raise ValueError("Parse error: Missing closing square brackets")
+        elif next_token == Token.LogicalGroup:
+            interior = self.handle_and_op()
+            expr = ExpressionLogicalGroup(next_token, right=interior)
+            next_token = self.next_token_is([Token.LogicalGroupEnd])
+            if next_token != Token.LogicalGroupEnd:
+                raise ValueError("Parse error: Missing closing paren")
+        elif next_token == Token.ContainingGroup:
+            interior = self.handle_and_op()
+            expr = ExpressionContainingGroup(next_token, right=interior)
+            next_token = self.next_token_is([Token.ContainingGroupEnd])
+            if next_token != Token.ContainingGroupEnd:
+                raise ValueError("Parse error: Missing closing square bracket")
+        else:
+            next_token = self.get_next_token()
+            if next_token:
+                expr = Expression(next_token)
+
+        return expr
+
+    def _parse(self, expression_string):
+        self.tokens = self._tokenize(expression_string)
+
+        expr = self.handle_and_op()
+
+        if self.at_token + 1 != len(self.tokens):
+            raise ValueError("Parse error in search string")
+
+        return expr
+
+    def _tokenize(self, expression_string):
+        grouping_re = r"\[\[|\[|,|\]\]|\]"
+        paren_re = "\)|\(|~"
+        word_re = r"\band\b|\bor\b|[_\-a-zA-Z0-9/.^#]+"
+        re_string = fr"({grouping_re}|{paren_re}|{word_re})"
+        token_re = re.compile(re_string)
+
+        tokens = token_re.findall(expression_string)
+        tokens = [Token(token) for token in tokens]
+
+        return tokens
+
+    def search_hed_string(self, hed_string_obj):
+        current_node = self.tree
+
+        result = current_node.handle_expr(hed_string_obj)
+        return result
+
+
+from hed.models.hed_string import HedStringFrozen
+
+
+def test_speed2():
+    expression = TagExpressionParser("[[a,b,[[c,d, [[e, f]]]]]]")
+    for i in range(100000):
+        hed_string_obj = HedStringFrozen("(a, b, ((e, f), c, d))")
+        expression.search_hed_string(hed_string_obj)
+
+
+if __name__ == '__main__':
+    expression = TagExpressionParser("[[a,b]]")
+    hed_string = HedStringFrozen("A, C, D, B")
+    print(expression.search_hed_string(hed_string), False)  # False
+    hed_string = HedStringFrozen("(A, B, C, D)")
+    print(expression.search_hed_string(hed_string), True)  # True
+    hed_string = HedStringFrozen("(A, C, D, B)")
+    print(expression.search_hed_string(hed_string), True)  # True
+
+    # Doesn't handle and/or precedence well(right to left and equal precedent currently)
+    expression = TagExpressionParser("a and b or c")
+    hed_string = HedStringFrozen("B")
+    print(expression.search_hed_string(hed_string), False)  # False
+    hed_string = HedStringFrozen("A, B")
+    print(expression.search_hed_string(hed_string), True)  # True
+    hed_string = HedStringFrozen("A, C")
+    print(expression.search_hed_string(hed_string), True)  # True
+    hed_string = HedStringFrozen("C")
+    print(expression.search_hed_string(hed_string), True)  # TRue
+
+    expression = TagExpressionParser("a or b and c")
+    hed_string = HedStringFrozen("B")
+    print(expression.search_hed_string(hed_string), False)  # False
+    hed_string = HedStringFrozen("A, B")
+    print(expression.search_hed_string(hed_string), False)  # False
+    hed_string = HedStringFrozen("A, C")
+    print(expression.search_hed_string(hed_string), True)  # True
+    hed_string = HedStringFrozen("C")
+    print(expression.search_hed_string(hed_string), False)  # false
+    hed_string = HedStringFrozen("A")
+    print(expression.search_hed_string(hed_string), False)  # false
+
+    expression = TagExpressionParser("a and (b or c)")
+    hed_string = HedStringFrozen("A, C")
+    print(expression.search_hed_string(hed_string), True)  # True
+
+    expression = TagExpressionParser("[[a,b]] or [[c, d]]")
+    hed_string = HedStringFrozen("(c, d)")
+    print(expression.search_hed_string(hed_string), True)  # True
+    hed_string = HedStringFrozen("(b, d)")
+    print(expression.search_hed_string(hed_string), False)  # False
+    hed_string = HedStringFrozen("(b, a)")
+    print(expression.search_hed_string(hed_string), True)  # True
+
+    expression = TagExpressionParser("[[a,b]] and [[c, d]]")
+    hed_string = HedStringFrozen("(c, d, a, b)")
+    print(expression.search_hed_string(hed_string), True)  # True
+    hed_string = HedStringFrozen("(c, d, a)")
+    print(expression.search_hed_string(hed_string), False)  # False
+    hed_string = HedStringFrozen("(c, d), (a, b)")
+    print(expression.search_hed_string(hed_string), True)  # True
+    breakHere = 3
+
+    expression = TagExpressionParser("[[a,b]] and d")
+    hed_string = HedStringFrozen("(a, b), d")
+    print(expression.search_hed_string(hed_string), True)  # True
+
+    expression = TagExpressionParser("[[a,b]] and d")
+    hed_string = HedStringFrozen("(a, b), c")
+    print(expression.search_hed_string(hed_string), False)  # False
+
+    expression = TagExpressionParser("[[a,b]] or d")
+    hed_string = HedStringFrozen("d")
+    print(expression.search_hed_string(hed_string), True)  # True
+
+    hed_string = HedStringFrozen("a, b")
+    print(expression.search_hed_string(hed_string), False)  # False
+
+    expression = TagExpressionParser("[[a,b,[[c,d]]]]")
+    # # Should this be valid?  It's iffy.
+    hed_string = HedStringFrozen("(a, b, c, d)")
+    print(expression.search_hed_string(hed_string), False)  # False
+
+    hed_string = HedStringFrozen("(a, b, (c, d))")
+    print(expression.search_hed_string(hed_string), True)  # True
+
+    expression = TagExpressionParser("[[a,b,[[c,d, [[e, f]]]]]]")
+    hed_string = HedStringFrozen("(a, b, ((e, f), c, d))")
+    print(expression.search_hed_string(hed_string), True)  # True
+
+    expression = TagExpressionParser("[[a,b,[[c,d, [[e, f]]]]]]")
+    hed_string = HedStringFrozen("(a, b, (e, f, c, d))")
+    print(expression.search_hed_string(hed_string), False)  # False
+
+    # this API needs work
+    # todo: clean up this trash with double layer strings.
+    from hed.models.hed_group import HedGroupFrozen
+
+    hed_string = HedStringFrozen("(a, b, ((e, f), c, d))")
+    group = hed_string.get_all_groups()[3]
+    search_string = HedStringFrozen("(f, e)")
+
+    # Need to figure out how to clean this up still
+    # This in particular seems wrong.  Why can I not search like this?
+    frozen_string = HedGroupFrozen(search_string.groups()[0])
+    print(frozen_string == group)
+    print(hed_string.find_exact_tags([frozen_string], recursive=True))
+
+    import cProfile
+    import time
+
+    start_time = time.time()
+    test_speed2()
+    time_taken = time.time() - start_time
+    print(f"time: {time_taken}")
+
+    start_time = time.time()
+    cProfile.run("test_speed2()", sort='cumtime')
+    time_taken = time.time() - start_time
+    print(f"time: {time_taken}")
+
