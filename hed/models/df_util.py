9a3e72585de75c22644ca1b50344e44febe84fd6
IanCa <ianrcallanan@gmail.com>
2023-04-03 20:50:36 -0500

    Refactor into class

--- hed/models/df_util.py
+++ hed/models/df_util.py
@@ -150,6 +150,7 @@ def _get_matching_value(tags):
 
     return next(iter(unique_values))
 
+
 def process_def_expands(hed_strings, hed_schema, known_defs=None, ambiguous_defs=None):
     """
     Processes a list of HED strings according to a given HED schema, using known definitions and ambiguous definitions.
@@ -163,83 +164,6 @@ def process_def_expands(hed_strings, hed_schema, known_defs=None, ambiguous_defs
         ambiguous_defs (dict): A dictionary containing ambiguous definitions
             format TBD.  Currently def name key: list of lists of hed tags values
     """
-    if not isinstance(hed_strings, pd.Series):
-        hed_strings = pd.Series(hed_strings)
-
-    if ambiguous_defs is None:
-        ambiguous_defs = {}
-    errors = {}
-    def_dict = DefinitionDict(known_defs)
-
-    def_expand_mask = hed_strings.str.contains('Def-Expand/', case=False)
-
-    # Iterate over the strings that contain def-expand tags
-    for i in hed_strings[def_expand_mask].index:
-        string = hed_strings.loc[i]
-        hed_str = HedString(string, hed_schema)
-
-        for def_tag, def_expand_group, def_group in hed_str.find_def_tags(recursive=True):
-            if def_tag == def_expand_group:
-                continue
-
-            def_tag_name = def_tag.extension.split('/')[0]
-            # First check for known definitions.  If this is known, it's done either way.
-            def_group_contents = def_dict._get_definition_contents(def_tag)
-            def_expand_group.sort()
-            if def_group_contents:
-                if def_group_contents != def_expand_group:
-                    errors.setdefault(def_tag_name.lower(), []).append(def_group)
-                continue
-
-            # This is a def we recognized earlier as an error AND it wasn't a known definition.
-            if def_tag_name.lower() in errors:
-                errors.setdefault(def_tag_name.lower(), []).append(def_group)
-                continue
-
-            has_extension = "/" in def_tag.extension
-
-            # If there's no extension, this is fine.
-            if not has_extension:
-                group_tag = def_expand_group.get_first_group()
-                def_dict.defs[def_tag_name.lower()] = DefinitionEntry(name=def_tag_name, contents=group_tag,
-                                                                      takes_value=False,
-                                                                      source_context=[])
-            else:
-                def_extension = def_tag.extension.split('/')[-1]
-                # Find any other tags in def_group.get_all_tags() with tags with the same extension
-                matching_tags = [tag for tag in def_expand_group.get_all_tags() if tag.extension == def_extension and tag != def_tag]
-
-                for tag in matching_tags:
-                    tag.extension = "#"
-
-                group_tag = def_expand_group.get_first_group()
-
-                these_defs = ambiguous_defs.setdefault(def_tag_name.lower(), [])
-                these_defs.append(group_tag)
-
-                value_per_tag = []
-                if len(these_defs) >= 1:
-                    all_tags_list = [group.get_all_tags() for group in these_defs]
-                    for tags in zip(*all_tags_list):
-                        matching_val = _get_matching_value(tags)
-                        value_per_tag.append(matching_val)
-
-                    if value_per_tag.count(None):
-                        groups = ambiguous_defs.get(def_tag_name.lower(), [])
-                        for group in groups:
-                            errors.setdefault(def_tag_name.lower(), []).append(group)
-
-                        del ambiguous_defs[def_tag_name.lower()]
-                        continue
-                    ambiguous_values = value_per_tag.count("#")
-                    if ambiguous_values == 1:
-                        new_contents = group_tag.copy()
-                        for tag, value in zip(new_contents.get_all_tags(), value_per_tag):
-                            if value is not None:
-                                tag.extension = f"{value}"
-                        def_dict.defs[def_tag_name.lower()] = DefinitionEntry(name=def_tag_name, contents=new_contents,
-                                                                              takes_value=True,
-                                                                              source_context=[])
-                        del ambiguous_defs[def_tag_name.lower()]
-
-    return def_dict, ambiguous_defs, errors
+    from hed.models.def_expand_gather import DefExpandGatherer
+    def_gatherer = DefExpandGatherer(hed_schema, known_defs, ambiguous_defs)
+    return def_gatherer.process_def_expands(hed_strings)
