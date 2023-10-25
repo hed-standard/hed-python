1d45491111e3de8bec12bd5500e05af8e05e05bc
Kay Robbins <1189050+VisLab@users.noreply.github.com>
2023-10-25 16:00:06 -0500

    Updated version in preparation for release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,8 +24,8 @@ copyright = '2017-{}, HED Working Group'.format(date.today().year)
 author = 'HED Working Group'
 
 # The full version, including alpha/beta/rc tags
-version = '0.3.1'
-release = '0.3.1'
+version = '0.4.0'
+release = '0.4.0'
 
 currentdir = os.path.realpath(os.path.dirname(__file__))
 
