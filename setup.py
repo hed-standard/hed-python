085e09c77c284a755e01b995d4de891bebdbf242
IanCa <ianrcallanan@gmail.com>
2024-02-02 16:09:39 -0600

    Restore setup.py, delete setup.cfg

---
+++ setup.py
@@ -0,0 +1,8 @@
+# dummy setup file for versioneer
+
+import versioneer
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(version=versioneer.get_version(),
+          cmdclass=versioneer.get_cmdclass())
