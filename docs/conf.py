cf6a1147fb7d92da4871babdde31afe9f365ede2
Kay Robbins <robbins.kay@gmail.com>
2026-05-08 12:35:26 -0500

    Updated README

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ copyright = "2024, HED Standard"
 author = "HED Standard"
 
 # The full version, including alpha/beta/rc tags
-release = "0.8.1"
+release = "1.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
